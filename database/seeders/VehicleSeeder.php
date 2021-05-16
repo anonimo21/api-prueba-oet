302ef7fc9e64eb9c2acac3cf5b2543bf7cd25f87
anonimo21 <anonnimo21@yahoo.es>
2021-05-15 23:19:27 -0500

    cambios en archivo de VehicleSeeder

--- database/seeders/VehicleSeeder.php
+++ database/seeders/VehicleSeeder.php
@@ -17,21 +17,21 @@ class VehicleSeeder extends Seeder
             'driver_id' => 1,
             'owner_id' => 3,
         ]);
-        Vehicle::create([
-            'placa' => 'XYQ457',
-            'color' => 'azul',
-            'marca' => 'toyota',
-            'type_id' => 1,
-            'driver_id' => 2,
-            'owner_id' => 3,
-        ]);
-        Vehicle::create([
-            'placa' => 'IYU790',
-            'color' => 'azul',
-            'marca' => 'chevrolet',
-            'type_id' => 2,
-            'driver_id' => 5,
-            'owner_id' => 4,
-        ]);
+        // Vehicle::create([
+        //     'placa' => 'XYQ457',
+        //     'color' => 'azul',
+        //     'marca' => 'toyota',
+        //     'type_id' => 1,
+        //     'driver_id' => 2,
+        //     'owner_id' => 3,
+        // ]);
+        // Vehicle::create([
+        //     'placa' => 'IYU790',
+        //     'color' => 'azul',
+        //     'marca' => 'chevrolet',
+        //     'type_id' => 2,
+        //     'driver_id' => 5,
+        //     'owner_id' => 4,
+        // ]);
     }
 }
