61784168e626b1f708b0c65752132f56404b98ee
anonimo21 <anonnimo21@yahoo.es>
2021-05-17 12:31:55 -0500

    ajustes en informe controller

--- app/Http/Controllers/InformeController.php
+++ app/Http/Controllers/InformeController.php
@@ -14,8 +14,8 @@ class InformeController extends Controller
             ->join('owners as o', 'o.person_id', 'v.owner_id')
             ->join('persons as pd', 'pd.id', 'd.person_id')
             ->join('persons as po', 'po.id', 'o.person_id')
-            //->select('v.placa','v.marca', DB::raw('CONCAT(pd.primer_nombre, " ", pd.segundo_nombre, " ", pd.apellidos) as conductor'), DB::raw('CONCAT(po.primer_nombre, " ", po.segundo_nombre, " ", po.apellidos) as propietario'))
-            ->select('v.placa','v.marca')
+            ->select('v.placa','v.marca', DB::raw('CONCAT(pd.primer_nombre, " ", pd.segundo_nombre, " ", pd.apellidos) as conductor'), DB::raw('CONCAT(po.primer_nombre, " ", po.segundo_nombre, " ", po.apellidos) as propietario'))
+            //->select('v.placa','v.marca')
             ->get();
         return response()->json($informe, 200);
     }
